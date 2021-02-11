a3cf0c1c6cb0c4510b334436ff690d84658ce6a4
gaowei1012 <gaomingwei2020@126.com>
2021-02-11 15:19:38 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.scss'
+import {Button} from 'antd'
+import {useDrag} from 'react-dnd'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello react
+      <Button type='primary'>HELLO</Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
