86eb3e6dfeb3cc3fc9a92818a4dfdfc6d1fa950c
gaowei1012 <gaomingwei2020@126.com>
2021-02-11 15:21:47 +0800

    add style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './app.scss'
 import {Button} from 'antd'
 import {useDrag} from 'react-dnd'
+import './style/app.scss'
 
 function App() {
   return (
     <div className="App">
-      hello react
+      hello react dnd 
       <Button type='primary'>HELLO</Button>
     </div>
   )
